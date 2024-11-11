f17e6bca027b94ea207bb91dc48b0b7a3d824dca
slava0135 <super.novalskiy_0135@inbox.ru>
2024-11-11 15:42:12 +0300

    setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use fs_wrap::{setup, teardown};
+use log::info;
 use rand::{rngs::StdRng, SeedableRng};
 
 mod abstract_fs;
@@ -9,15 +10,20 @@ mod fs_wrap;
 mod mutator;
 
 fn main() {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    info!("starting up");
+
     let mut rng = StdRng::seed_from_u64(123);
     let seq = mutator::generate_new(&mut rng, 100);
     println!("{}", encode::encode_c(seq));
     setup(
         Path::new("/mnt").join("ext4").join("fstest").as_path(),
         fs_wrap::FileSystemType::EXT4,
-    ).unwrap();
+    )
+    .unwrap();
     teardown(
         Path::new("/mnt").join("ext4").join("fstest").as_path(),
         fs_wrap::FileSystemType::EXT4,
-    ).unwrap();
+    )
+    .unwrap();
 }
