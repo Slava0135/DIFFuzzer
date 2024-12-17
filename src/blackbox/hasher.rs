8b5fafe770279ec94dd3668c542d15b0c9bfe043
slava0135 <super.novalskiy_0135@inbox.ru>
2024-12-17 12:55:53 +0300

    refactor(hasher): HasherError

--- src/blackbox/hasher.rs
+++ src/blackbox/hasher.rs
@@ -1,45 +1,47 @@
-use log::warn;
 use std::path::Path;
 use std::process::Command;
-use std::str;
 
 pub struct Hasher {
     pub path: Box<Path>,
     pub options: String,
 }
 
+enum HasherError {
+    Eval(String),
+}
+
 impl Hasher {
     pub fn compare(&self, fst_path: &Path, snd_path: &Path) {
-        let hash_target = self.eval(fst_path);
-        let hash_reference = self.eval(snd_path);
-        if hash_target != hash_reference {
-            Command::new(self.path.as_ref())
-                .arg(&self.options)
-                .arg("-d")
-                .arg(fst_path)
-                .arg(snd_path)
-                .output()
-                .expect("error when comparing hashes");
+        let fst_hash = self.eval(fst_path);
+        let snd_hash = self.eval(snd_path);
+        match (fst_hash, snd_hash) {
+            (Ok(fst_hash), Ok(snd_hash)) if fst_hash != snd_hash => {
+                Command::new(self.path.as_ref())
+                    .arg(&self.options)
+                    .arg("-d")
+                    .arg(fst_path)
+                    .arg(snd_path)
+                    .output()
+                    .expect("error when comparing hashes");
+            }
+            _ => {}
         }
     }
 
-    fn eval(&self, path: &Path) -> Vec<u8> {
+    fn eval(&self, path: &Path) -> Result<String, HasherError> {
         let output = Command::new(self.path.as_ref())
             .arg(&self.options)
             .arg(path)
             .output()
             .expect("error when evaluating hash");
         if !output.status.success() {
-            let err_str = match str::from_utf8(&output.stderr) {
-                Ok(val) => val,
-                Err(_) => panic!("got non UTF-8 data from stderr"),
-            };
-            warn!(
-                "failed to eval abstract state for filesystem {}:{}",
-                path.display(),
-                err_str
-            );
+            return Err(HasherError::Eval(
+                String::from_utf8(output.stderr).unwrap_or("error reading stderr".to_owned()),
+            ));
         }
-        return output.stdout;
+        match String::from_utf8(output.stdout) {
+            Ok(hash) => Ok(hash),
+            Err(err) => Err(HasherError::Eval(err.to_string())),
+        }
     }
 }
