a3359e56aacef479f2eaca6cf018ae175f54eb08
kechinvv <valeraghst@gmail.com>
2025-01-09 14:19:33 +0300

    skip nfs temp files

--- src/hasher/hasher.rs
+++ src/hasher/hasher.rs
@@ -3,9 +3,8 @@ use std::hash::Hasher;
 use std::os::unix::fs::MetadataExt;
 use std::path::Path;
 use walkdir::WalkDir;
-
+use std::sync::OnceLock;
 use twox_hash::XxHash64;
-
 
 
 // if nlink = True, include nlink to hash. Same for mode.
@@ -14,11 +13,12 @@ pub fn get_hash_for_dir(path: &Path, seed: u64, nlink: bool, mode: bool) -> u64 
 
     for entry in WalkDir::new(path).sort_by(|a, b| a.file_name().cmp(b.file_name())) {
         let entry = entry.unwrap();
+        let strip_path= entry.path().strip_prefix(path).unwrap().to_str().unwrap();
 
-        //todo: skip nfs temp files
+        if is_nfs_tmp(strip_path) { continue; }
+
         let metadata = entry.metadata().unwrap();
-
-        hasher.write(entry.path().strip_prefix(path).unwrap().to_str().unwrap().as_bytes());
+        hasher.write(strip_path.as_bytes());
         hasher.write(&metadata.gid().to_le_bytes());
         hasher.write(&metadata.uid().to_le_bytes());
         if nlink { hasher.write(&metadata.nlink().to_le_bytes()); }
@@ -30,24 +30,19 @@ pub fn get_hash_for_dir(path: &Path, seed: u64, nlink: bool, mode: bool) -> u64 
     return hasher.finish();
 }
 
-//todo: check valid
-const NTFS_INTERNAL_DIRS: HashSet<&str> = HashSet::from(["/lost+found", "/.nilfs", "/.mcfs_dummy"]);
 
-fn is_nfs_tmp(path: &Path) -> bool {
-    return NTFS_INTERNAL_DIRS.contains(path.to_str().unwrap());
+fn get_ntfs_internal_dirs() -> &'static HashSet<&'static str> {
+    static HASHSET: OnceLock<HashSet<&str>> = OnceLock::new();
+    HASHSET.get_or_init(|| {
+        let mut m = HashSet::new();
+        m.insert("/lost+found");
+        m.insert("/.nilfs");
+        m.insert("/.mcfs_dummy");
+        m
+    })
 }
 
-/*
-std::unordered_set<std::string> exclusion_list = {
-        {"/lost+found"},
-        {"/.nilfs"},
-        {"/.mcfs_dummy"},
-        {"/build"}
-};
 
-/* Also ignore NFS temp files "/.nfsXXXX" */
-static inline bool is_excluded(const std::string &path) {
-    return (exclusion_list.find(path) != exclusion_list.end() || path.rfind("./nfs", 0) == 0);
+fn is_nfs_tmp(path: &str) -> bool {
+    return get_ntfs_internal_dirs().contains(path) || path.starts_with("/.nfs");
 }
-
-*/
