c30e6fd7d3d516089dcb920c714b1b2c9ac95767
slava0135 <super.novalskiy_0135@inbox.ru>
2025-01-15 13:02:48 +0300

    feat: xfs mount

---
+++ src/mount/xfs.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+use super::mount::FileSystemMount;
+
+pub struct XFS;
+
+impl Display for XFS {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "XFS")
+    }
+}
+
+impl FileSystemMount for XFS {
+    fn mkfs_cmd(&self) -> String {
+        "mkfs.xfs".to_owned()
+    }
+    fn mount_t(&self) -> String {
+        "xfs".to_owned()
+    }
+}
+
+impl XFS {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
