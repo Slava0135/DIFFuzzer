6c8e2f8fcc5bbefdc76d080ffb7620e5b80de88b
slava0135 <super.novalskiy_0135@inbox.ru>
2025-01-20 14:20:38 +0300

    tests(abstract_fs): `Content` tests

--- src/abstract_fs/content.rs
+++ src/abstract_fs/content.rs
@@ -37,7 +37,7 @@ impl Content {
         }
     }
 
-    pub fn write(&mut self, src_offset: u64, offset: u64, size: u64) {
+    pub fn write(&mut self, src_offset: u64, write_offset: u64, size: u64) {
         let old_sise = self.size();
         if size > 0 {
             let mut truncate_size = size;
@@ -95,7 +95,10 @@ impl Content {
                 if read_size >= size {
                     break;
                 }
-                let slice_read_size = if s.size() > size { size } else { s.size() };
+                let mut slice_read_size = s.size();
+                if read_size + slice_read_size > size {
+                    slice_read_size = size - read_size;
+                }
                 content.write_back(s.from, slice_read_size);
                 read_size += slice_read_size;
             } else {
@@ -120,5 +123,38 @@ impl Content {
 
     pub fn size(&self) -> u64 {
         self.slices.iter().fold(0, |acc: u64, s| acc + s.size())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Content;
+
+    #[test]
+    fn test_read_empty() {
+        let content = Content::new();
+        assert_eq!(Content::new().slices(), content.read(0, 0).slices());
+    }
+
+    #[test]
+    fn test_read_from_start() {
+        let mut content = Content::new();
+        content.write_back(42, 100);
+        content.write_back(1, 2);
+        content.write_back(13, 55);
+        assert_eq!(content, content.read(0, 1024));
+    }
+
+    #[test]
+    fn test_read_from_middle() {
+        let mut content = Content::new();
+        content.write_back(42, 100);
+        content.write_back(1, 2);
+        content.write_back(13, 55);
+        let mut expected = Content::new();
+        expected.write_back(42 + 95, 5);
+        expected.write_back(1, 2);
+        expected.write_back(13, 10 - 5 - 2);
+        assert_eq!(expected, content.read(95, 10));
     }
 }
