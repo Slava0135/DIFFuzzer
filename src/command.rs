b44cb925da4a2f04c2662665c03034e80e8c547f
slava0135 <super.novalskiy_0135@inbox.ru>
2025-02-05 14:48:41 +0300

    feat: RemoteCommandInterface functions

--- src/command.rs
+++ src/command.rs
@@ -7,6 +7,8 @@ use std::{
 
 use anyhow::{bail, Context, Ok};
 
+use crate::config::QemuConfig;
+
 pub trait CommandInterface {
     fn create_dir_all(&self, path: &Path) -> anyhow::Result<()>;
     fn remove_dir_all(&self, path: &Path) -> anyhow::Result<()>;
@@ -29,6 +31,20 @@ impl CommandWrapper {
     pub fn arg<S: AsRef<OsStr>>(&mut self, arg: S) -> &mut Self {
         self.internal.arg(arg);
         self
+    }
+    fn exec_local(mut self) -> anyhow::Result<Output> {
+        let output = self
+            .internal
+            .output()
+            .with_context(|| format!("failed to run local command: {:?}", self.internal))?;
+        if !output.status.success() {
+            bail!(
+                "local command {:?} execution ended with error:\n{}",
+                self.internal,
+                String::from_utf8(output.stderr).unwrap_or("<invalid UTF-8 string>".into())
+            );
+        }
+        Ok(output)
     }
 }
 
@@ -69,44 +85,72 @@ impl CommandInterface for LocalCommandInterface {
         })?;
         Ok(())
     }
-    fn exec(&self, mut cmd: CommandWrapper) -> anyhow::Result<Output> {
-        let output = cmd
-            .internal
-            .output()
-            .with_context(|| format!("failed to run local command: {:?}", cmd.internal))?;
-        if !output.status.success() {
-            bail!(
-                "local command {:?} execution ended with error:\n{}",
-                cmd.internal,
-                String::from_utf8(output.stderr).unwrap_or("<invalid UTF-8 string>".into())
-            );
-        }
-        Ok(output)
+    fn exec(&self, cmd: CommandWrapper) -> anyhow::Result<Output> {
+        cmd.exec_local()
     }
 }
 
-pub struct RemoteCommandInterface {}
+pub struct RemoteCommandInterface {
+    config: QemuConfig,
+}
 
 impl RemoteCommandInterface {
-    pub fn new() -> Self {
-        RemoteCommandInterface {}
+    pub fn new(config: QemuConfig) -> Self {
+        RemoteCommandInterface { config }
     }
 }
 
 impl CommandInterface for RemoteCommandInterface {
     fn create_dir_all(&self, path: &Path) -> anyhow::Result<()> {
-        todo!()
+        let mut mkdir = CommandWrapper::new("mkdir");
+        mkdir.arg("-p");
+        mkdir.arg(path);
+        self.exec(mkdir)
+            .with_context(|| format!("failed to create remote dir at '{}'", path.display()))?;
+        Ok(())
     }
     fn remove_dir_all(&self, path: &Path) -> anyhow::Result<()> {
-        todo!()
+        let mut rm = CommandWrapper::new("rm");
+        rm.arg("-rf");
+        rm.arg(path);
+        self.exec(rm)
+            .with_context(|| format!("failed to remove remote dir at '{}'", path.display()))?;
+        Ok(())
     }
     fn copy_to_guest(&self, host_path: &Path, guest_path: &Path) -> anyhow::Result<()> {
-        todo!()
+        let mut scp = self.copy_common();
+        scp.arg(host_path);
+        scp.arg(format!("root@localhost:{}", guest_path.display()));
+        scp.exec_local()?;
+        Ok(())
     }
     fn copy_to_host(&self, guest_path: &Path, host_path: &Path) -> anyhow::Result<()> {
-        todo!()
+        let mut scp = self.copy_common();
+        scp.arg(format!("root@localhost:{}", guest_path.display()));
+        scp.arg(host_path);
+        scp.exec_local()?;
+        Ok(())
     }
     fn exec(&self, cmd: CommandWrapper) -> anyhow::Result<Output> {
-        todo!()
+        let mut ssh = CommandWrapper::new("ssh");
+        ssh.arg("-q");
+        ssh.arg("-i").arg(self.config.ssh_private_key_path.clone());
+        ssh.arg("-o").arg("StrictHostKeyChecking no");
+        ssh.arg("-p").arg(self.config.ssh_port.to_string());
+        ssh.arg("root@localhost");
+        ssh.arg("-t").arg(format!("{:?}", cmd.internal));
+        ssh.exec_local()
+    }
+}
+
+impl RemoteCommandInterface {
+    fn copy_common(&self) -> CommandWrapper {
+        let mut scp = CommandWrapper::new("scp");
+        scp.arg("-q");
+        scp.arg("-i").arg(self.config.ssh_private_key_path.clone());
+        scp.arg("-o").arg("StrictHostKeyChecking no");
+        // not a typo
+        scp.arg("-P").arg(self.config.ssh_port.to_string());
+        scp
     }
 }
