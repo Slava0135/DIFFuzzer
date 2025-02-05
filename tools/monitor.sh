33e12514344da84779c9550a7d43e4fa331be3b4
slava0135 <super.novalskiy_0135@inbox.ru>
2025-02-05 12:37:25 +0300

    feat(tools): update vm tools

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -2,19 +2,4 @@
 
 : "${MONITOR_PORT:="55555"}"
 
-function powerdown() {
-    echo powering down VM...
-    echo system_powerdown | netcat -N localhost "$MONITOR_PORT"
-}
-
-function savevm() {
-    echo saving vm snapshot with tag "$2"
-    echo "savevm $2" | netcat -N localhost "$MONITOR_PORT"
-}
-
-function loadvm() {
-    echo loading vm snapshot with tag "$2"
-    echo "loadvm $2" | netcat -N localhost "$MONITOR_PORT"
-}
-
-FUNC_CALL=$1; shift; $FUNC_CALL "$@"
+echo "$@" | netcat -N localhost "$MONITOR_PORT"
