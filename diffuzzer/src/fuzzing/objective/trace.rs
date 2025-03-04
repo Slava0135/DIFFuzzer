9f76daaf8282f9523c358910d4850543499395f7
kechinvv <valeraghst@gmail.com>
2025-03-04 18:26:42 +0300

    feat: in progress reducer, get_diff for trace

--- diffuzzer/src/fuzzing/objective/trace.rs
+++ diffuzzer/src/fuzzing/objective/trace.rs
@@ -3,8 +3,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use log::debug;
+use dash::FileInfo;
 
-use crate::abstract_fs::trace::Trace;
+use crate::abstract_fs::trace::{Trace, TraceRow};
+use crate::fuzzing::objective::trace::TraceDiff::{DifferentLength, ExitCodeIsDifferent};
 
 pub struct TraceObjective {}
 
@@ -14,9 +16,35 @@ impl TraceObjective {
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum TraceDiff {
+    ExitCodeIsDifferent { fst: TraceRow, snd: TraceRow },
+    DifferentLength,
+}
+
 impl TraceObjective {
     pub fn is_interesting(&mut self, fst_trace: &Trace, snd_trace: &Trace) -> anyhow::Result<bool> {
         debug!("do trace objective");
         Ok(!fst_trace.same_as(snd_trace))
+    }
+
+    pub fn get_diff(&mut self, fst_trace: &Trace, snd_trace: &Trace) -> Vec<TraceDiff> {
+        let mut trace_diff: Vec<TraceDiff> = vec![];
+
+        if fst_trace.rows.len() != snd_trace.rows.len() {
+            trace_diff.push(DifferentLength);
+            return trace_diff;
+        }
+
+        for i in 0..fst_trace.rows.len() {
+            let mut fst_row = fst_trace.rows[i].clone();
+            let mut snd_row = snd_trace.rows[i].clone();
+            if fst_row != snd_row {
+                fst_row.index = 0;
+                snd_row.index = 0;
+                trace_diff.push(ExitCodeIsDifferent {fst: fst_row, snd: snd_row})
+            }
+        }
+        return trace_diff;
     }
 }
